b08a0847d54e26cf71376b8204cd761f222a7375
Michael Petersen <development@michaelpetersen.co>
2022-07-30 13:24:51 +1000

    Update media_install.sh

--- media_install.sh
+++ media_install.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
+read -p "Install otf-font-awesome? [y/N] " install
 
 ## Install Cargo
 curl https://sh.rustup.rs -sSf | sh
 
-## Install playerctl
-read -p "Install otf-font-awesome? [y/N] " install
 
+## Install playerctl
 sudo pacman -S playerctl --needed --noconfirm
-if [ $install == "y" ]; then
+if [ "$install" == "y" ]; then
  sudo pacman -S otf-font-awesome --needed --noconfirm
 fi
 
