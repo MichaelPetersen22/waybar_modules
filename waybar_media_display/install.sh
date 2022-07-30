77cae0c5844b5f27ce37debe2f6f9b10420136d8
MichaelPetersen22 <development@michaelpetersen.co>
2022-07-30 12:18:12 +1000

    Added Install Script

---
+++ waybar_media_display/install.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+## Install Cargo
+curl https://sh.rustup.rs -sSf | sh
+## Install playerctl
+sudo pacman -S playerctl
+
+## Install Module
+cargo install waybar_media_display
+
+echo -e "Waybar Media Display Module Installed.
+Run waybar_media_display --help for options"
