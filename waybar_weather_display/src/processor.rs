b4cb0843b8041399746117e26d336cd72ef2f9ff
MichaelPetersen22 <development@michaelpetersen.co>
2022-08-25 15:15:47 +1000

    Added Icons and weathers

--- waybar_weather_display/src/processor.rs
+++ waybar_weather_display/src/processor.rs
@@ -1,48 +1,31 @@
-// Weather Codes https://open-meteo.com/en/docs
-const CLEAR: u8 = 0;
-const CLEARICO: char = '';
-
-// TODO Implement Icon
-const PARTLYCLEAR: u8 = 1;
-const PARTICO: char = '';
-
-const FOGGY: [u8; 2] = [45, 48];
-const FOGICO: char = 'F';
-
-
-// TODO Implement Icon
-// [0] Partly Cloudy, [1] Overcast
-const CLOUDY: [u8; 2] = [2, 3];
-const CLOUDICO: char = '';
-
-// [0] Light, [1] Moderate, [2] Dense, [3] Freezing light, [4] Freezing Dense
+// Codes
+const CLEAR: [u8;2] = [0,1];
+const PARTLY_CLOUDY: u8 = 2;
+const OVERCAST: u8 = 3;
+const FOG: u8 = 45;
+const RIMEFOG: u8 = 48;
 const DRIZZLE: [u8; 5] = [51, 53, 55, 56, 57];
-const DRIZZICO: char = 'D';
-
-// [0] Slight, [1] Moderate, [2] Heavy, [3] Freezing Light, [4] Freezing Dense
 const RAIN: [u8; 5] = [61, 63, 65, 66, 67];
-const RAINICO: char = '';
+const SNOW: [u8; 3] = [77, 85, 86];
+const SHOWERS: [u8; 3] = [80, 81, 82];
+const THUNDERSTORM: u8 = 95;
+const HAILSTORM: [u8; 2] = [96,99];
 
-// [0] Slight, [1] Moderate, [2] Heavy, [3] Grains
-const SNOW: [u8; 4] = [71, 73, 75, 77];
+
+// Icons
+const CLEARICO: char = '';
+const PARTLY_CLOUDYICO: char = '杖';
+const OVERCASTICO: char = '摒';
+const FOGICO: char = '';
+const RIMEFOGICO: char = '敖';
+const DRIZZLEICO: char = '';
+const RAINICO: char = '';
 const SNOWICO: char = '';
-
-// Rain Showers
-// [0] Slight, [1] Moderate, [2] Heavy
-const RSHOWERS: [u8; 3] = [80, 81, 82];
-const RSHOWICO: char = '';
-
-// Snow Showers
-// [0] Slight, [1] Heavy
-const SNSHOWERS: [u8; 2] = [85, 86];
-const SNSHOWICO: char = '';
-
-// [0] Slight or Moderate, [1] Slight Hail, [2] Heavy Hail
-const THUNDERSTORM: [u8; 3] = [95, 96, 99];
-const THUNDERICO: char = '';
-
-// Error Icon when code does not return
+const SHOWERSICO: char = '';
+const THUNDERSTORMICO: char = '';
+const HAILICO: char = '';
 const ERRICO: char = '';
+
 
 pub struct Weather {
     pub icon: char,
@@ -57,53 +40,56 @@ pub fn parse_weather(code: u8) -> Weather{
 }
 
 fn ico(code: &u8) -> char {
-    if CLEAR == code.to_owned() {
+    if CLEAR.contains(&code){
         return CLEARICO;
-    } else if PARTLYCLEAR == code.to_owned() {
-        return PARTICO;
-    } else if FOGGY.contains(&code) {
-        return FOGICO;
-    } else if CLOUDY.contains(&code) {
-        return CLOUDICO;
     } else if DRIZZLE.contains(&code) {
-        return DRIZZICO;
-    } else if RAIN.contains(&code) {
+        return DRIZZLEICO;
+    } else if RAIN.contains(&code){
         return RAINICO;
-    } else if SNOW.contains(&code) {
+    } else if SNOW.contains(&code){
         return SNOWICO;
-    } else if RSHOWERS.contains(&code) {
-        return RSHOWICO;
-    } else if SNSHOWERS.contains(&code) {
-        return SNSHOWICO;
-    } else if THUNDERSTORM.contains(&code) {
-        return THUNDERICO;
-    } else {
-        return ERRICO;
+    } else if SHOWERS.contains(&code) {
+        return SHOWERSICO;
+    } else if HAILSTORM.contains(&code){
+        return HAILICO;
     }
+
+    let co = code.to_owned();
+    let output: char = match co {
+        PARTLY_CLOUDY => return PARTLY_CLOUDYICO,
+        OVERCAST => return OVERCASTICO,
+        FOG => return FOGICO,
+        RIMEFOG => return RIMEFOGICO,
+        THUNDERSTORM => return THUNDERSTORMICO,
+        _=> return ERRICO,
+    };
+
+    return output;
 }
 
 fn text(code: u8) -> String {
-    if CLEAR == code {
+    if CLEAR.contains(&code){
         return "Clear".to_string();
-    } else if PARTLYCLEAR == code {
-        return "Partly Cloudy".to_string();
-    } else if FOGGY.contains(&code) {
-        return "Foggy".to_string();
-    } else if CLOUDY.contains(&code) {
-        return "Cloudy".to_string();
     } else if DRIZZLE.contains(&code) {
         return "Drizzle".to_string();
-    } else if RAIN.contains(&code) {
+    } else if RAIN.contains(&code){
         return "Raining".to_string();
-    } else if SNOW.contains(&code) {
-        return "Snowy".to_string();
-    } else if RSHOWERS.contains(&code) {
-        return "Rain Showers".to_string();
-    } else if SNSHOWERS.contains(&code) {
-        return "Snow Showers".to_string();
-    } else if THUNDERSTORM.contains(&code) {
-        return "Thunder Storms".to_string();
-    } else {
-        return "Weather Error".to_string();
+    } else if SNOW.contains(&code){
+        return "Snowing".to_string();
+    } else if SHOWERS.contains(&code) {
+        return "Showers".to_string();
+    } else if HAILSTORM.contains(&code){
+        return "Hail".to_string();
     }
+
+    let output: String = match code {
+        PARTLY_CLOUDY => return "Partly Cloudy".to_string(),
+        OVERCAST => return "Overcast".to_string(),
+        FOG => return "Foggy".to_string(),
+        RIMEFOG => return "Rime Fog".to_string(),
+        THUNDERSTORM => return "Thunderstorms".to_string(),
+        _=> return "Weather Error".to_string(),
+    };
+
+    return output;
 }
